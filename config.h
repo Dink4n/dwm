1e27793343a45e213540f09f379209b7f971b790
JimboKearn <anujithbahuleyan@gmail.com>
2021-02-01 02:36:58 +0530

    ShiftView(modified) patch

--- config.h
+++ config.h
@@ -92,6 +92,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ ControlMask|Mod1Mask,         XK_Right,  shiftviewclients, { .i = +1 } },
+	{ ControlMask|Mod1Mask,         XK_Left,   shiftviewclients, { .i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
