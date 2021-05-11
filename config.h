7f0a6af7ee41e21c9432991d374fff760120d2d7
JimboKearn <anujithbahuleyan@gmail.com>
2021-05-11 12:41:41 +0530

    Swallow patch

--- config.h
+++ config.h
@@ -28,8 +28,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 8,       1,           -1 },
+	/* class      instance    title       tags mask     isfloating   isterminal noswallow monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           0,         0,        -1 },
+	{ "St",       NULL,       NULL,       0,            0,           1,         1,        -1 },
 };
 
 /* layout(s) */
