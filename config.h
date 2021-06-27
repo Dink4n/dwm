975975ec08f153f5a147035c80638cd704833f5f
JimboKearn <anujithbahuleyan@gmail.com>
2021-06-27 17:25:42 +0530

    VanityGaps patch

--- config.h
+++ config.h
@@ -2,8 +2,12 @@
 
 /* appearance */
 static unsigned int borderpx  = 4;        /* border pixel of windows */
-static unsigned int gappx     = 14;       /* gaps between windows */
 static unsigned int snap      = 32;       /* snap pixel */
+static unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
@@ -30,10 +34,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
-	{ "Game",    NULL,     NULL,           0,         1,          0,           1,        -1 },
+	{ "Gimp",			NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "St",      		NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      		NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	{ "Game",    		NULL,     NULL,           0,         1,          0,           1,        -1 },
+    { "glslViewer",     NULL,     NULL,           0,         1,          0,           1,        -1 },
+	{ "Hdajackretask",  NULL,     NULL,           0,         0,          0,           1,        -1 },
 };
 
 /* layout(s) */
@@ -41,15 +47,21 @@ static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 1;    /* number of clients in master area */
 static int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
-#include "fibonacci.c"
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },       /* first entry is default */
+	{ "[]=",      tile },       /* Master on left, slaves on right */
+    { "TTT",      bstack },     /* Master on top, slaves on bottom */
 
  	{ "[M]",	  monocle },    /* All windows on top of eachother */
+    { "H[]",      deck },       /* Master on left, monocle mode on right */
 
 	{ "[@]",	  spiral },	    /* Fibonacci spiral */
 	{ "[\\]",	  dwindle },    /* Decreasing in size right and leftward */
+
+    { "|M|",      centeredmaster }, /* Master in middle, slaves on sides */
+    { ">M>",      centeredfloatingmaster }, /* Same as centeredmaster, but master floats */
 
 	{ "><>",      NULL },       /* no layout function means floating behavior */
 };
@@ -126,15 +138,20 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 
     /* Gaps */
-	{ MODKEY|ControlMask,           XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ControlMask,           XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_0,      setgaps,        {.i = 0  } },
+    { MODKEY,                       XK_g,      defaultgaps,    {0} },
+    { MODKEY|ShiftMask,             XK_g,      togglegaps,     {0} },
+	{ MODKEY|ShiftMask,             XK_equal,  incrgaps,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_minus,  incrgaps,       {.i = -1 } },
 
     /* Layouts */
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, /* tile */
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} }, /* monocle */
-	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[2]} }, /* spiral */
-	{ MODKEY|ShiftMask,             XK_y,      setlayout,      {.v = &layouts[3]} }, /* dwindle */
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[1]} }, /* bstack */
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, /* monocle */
+	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[3]} }, /* deck */
+	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[4]} }, /* spiral */
+	{ MODKEY|ShiftMask,             XK_y,      setlayout,      {.v = &layouts[5]} }, /* dwindle */
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[6]} }, /* centeredmaster */
+	{ MODKEY|ShiftMask,             XK_u,      setlayout,      {.v = &layouts[7]} }, /* centerdfloatingmaster */
 	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefloating, {0} },
 
